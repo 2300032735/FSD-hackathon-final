9b8e90b756bd49c0c48c969b5f3bd9d4b689361c
NEELAKSHI SRAVYA <163744895+2300032735@users.noreply.github.com>
2025-05-11 08:23:59 +0530

    Add files via upload

---
+++ Foodify/tailwind.config.js
@@ -0,0 +1,29 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#fce7f3',
+          200: '#fbcfe8',
+          300: '#f9a8d4',
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+        },
+        secondary: {
+          100: '#f3e8ff',
+          200: '#e9d5ff',
+          300: '#d8b4fe',
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
